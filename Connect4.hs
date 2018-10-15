d13313dbf767117e708d90b72725065c4c5304da
w9g0b <stephen.hou.53@gmail.com>
2018-10-14 18:43:43 -0700

    fix computer player bugs

--- Connect4.hs
+++ Connect4.hs
@@ -93,65 +93,68 @@ isOutOfBound i j = i > 5 || j > 6 || i < 0 || j < 0
 computer :: Opponent
 -- this player has an ordering of the moves, and chooses the first one available
 computer state playerMove prevMove = 
-    let winningMove = if prevMove /= -1 then findMandatoryMove state prevMove 'O' else -1
-        nextMove = if playerMove /= -1 then findMandatoryMove state playerMove 'X' else 6 -- TODO: implement regular circumstance placement strategy
+    let offset = if prevMove == playerMove then 1 else 0
+        winningMove = if prevMove /= -1 then findMandatoryMove state prevMove 'O' offset else (-1)
+        nextMove = if playerMove /= -1 then findMandatoryMove state playerMove 'X' 0 else (-1)
     in if winningMove /= -1 then winningMove
-         else nextMove
+         else if nextMove /= -1 then nextMove
+         else 6 -- TODO: implement regular circumstance placement strategy
 
--- can be used to find the winning move for the computer/find the position to block to prevent human from winning
-findMandatoryMove :: State -> Int -> Char -> Int
-findMandatoryMove (State board colPos) playerMove player = 
-    let horzPos = (findWinningGap player
-                    (checkStringHelper (State board colPos) 0 (colPos !! playerMove) (playerMove+3) (\ i -> i) (\ j -> j-1) [])
-                    0 0
-                  )
-        diagDownPos = (findWinningGap player
-                        (checkStringHelper (State board colPos) 0 ((colPos !! playerMove)+3) (playerMove+3) (\ i -> i-1) (\ j -> j-1) [])
-                        0 0
-                      )
-        diagUpPos = (findWinningGap player
-                        (checkStringHelper (State board colPos) 0 ((colPos !! playerMove)-3) (playerMove+3) (\ i -> i+1) (\ j -> j-1) [])
-                        0 0
-                      )
-        vertPos = (findWinningGap player
-                    (checkStringHelper (State board colPos) 3 ((colPos !! playerMove)+3) (playerMove) (\ i -> i-1) (\ j -> j) [])
-                    0 0
-                  )
+-- can be used to find the winning move for the computer
+-- OR
+-- find the position to block to prevent human from winning
+findMandatoryMove :: State -> Int -> Char -> Int -> Int
+findMandatoryMove (State board colPos) playerMove player offset = 
+    let horzCheckStr = (checkStringHelper (State board colPos) 0 (((colPos !! playerMove)+1)+offset) (playerMove+3) (\ i -> i) (\ j -> j-1) [])
+        diagDownCheckStr = (checkStringHelper (State board colPos) 0 (((colPos !! playerMove)+4)+offset) (playerMove+3) (\ i -> i-1) (\ j -> j-1) [])
+        diagUpCheckStr = (checkStringHelper (State board colPos) 0 (((colPos !! playerMove)-2)+offset) (playerMove+3) (\ i -> i+1) (\ j -> j-1) [])
+        horzPos = (findWinningGap player horzCheckStr playerMove 0 (length horzCheckStr) 0)
+        diagDownPos = (findWinningGap player diagDownCheckStr playerMove 0 (length diagDownCheckStr) 0)
+        diagUpPos = (findWinningGap player diagUpCheckStr playerMove 0 (length diagUpCheckStr) 0)
     in if horzPos /= -1 then horzPos
          else if diagDownPos /= -1 then diagDownPos
          else if diagUpPos /= -1 then diagUpPos
-         else vertPos
+         else if (vertWin (State board colPos) playerMove player) && (offset == 0) then playerMove
+         else -1
 
 
 -- identifies from the human's last move the vertical/horizontal/diagonal section that could now be 1 move away from winning
 checkStringHelper :: State -> Int -> Int -> Int -> (Int -> Int) -> (Int -> Int) -> [Char] -> [Char]
 checkStringHelper (State board colPos) count i j fi fj acc
-    | (count == 8) = acc
-    | isOutOfBound i j = checkStringHelper (State board colPos) (count+1) (fi i) (fj j) fi fj acc
+    | (count == 7) = acc
+    | isOutOfBound i j = checkStringHelper (State board colPos) (count+1) (fi i) (fj j) fi fj (['@'] ++ acc) -- signals out of bounds
     | otherwise = checkStringHelper (State board colPos) (count+1) (fi i) (fj j) fi fj (elt ++ acc)
     where elt = if ((colPos !! j) <= i) then [(board !! i !! j)] else ['#'] -- if this spot isn't actually reachable on the next move, disregard
 
 -- uses check string to find a gap that could win the game for the input player
-findWinningGap :: Char -> [Char] -> Int -> Int -> Int
-findWinningGap player checkStr pos prevMatches
-    | (pos == 7) = -1
-    | ((checkStr !! pos) == player) = findWinningGap player checkStr (pos+1) (prevMatches+1)
-    | ((checkStr !! pos) == '*') = if (checkNextInCheckStr player checkStr (pos+1) (3-prevMatches)) then pos else findWinningGap player checkStr (pos+1) 0
-    | otherwise = findWinningGap player checkStr (pos+1) 0
+findWinningGap :: Char -> [Char] -> Int -> Int -> Int -> Int -> Int
+findWinningGap player checkStr realPos pos len prevMatches 
+    | (pos == len) = -1
+    | ((checkStr !! pos) == player) = findWinningGap player checkStr realPos (pos+1) len (prevMatches+1)
+    | ((checkStr !! pos) == '*') = if (checkNextInCheckStr player checkStr (pos+1) len (3-prevMatches)) then (realPos + (pos-3)) else findWinningGap player checkStr realPos (pos+1) len 0
+    | otherwise = findWinningGap player checkStr realPos (pos+1) len 0
 
-checkNextInCheckStr :: Char -> [Char] -> Int -> Int -> Bool
-checkNextInCheckStr player checkStr pos checksLeft
+checkNextInCheckStr :: Char -> [Char] -> Int -> Int -> Int -> Bool
+checkNextInCheckStr player checkStr pos len checksLeft
     | (checksLeft == 0) = True
-    | (pos >= 7) = False
-    | otherwise = currCheck && checkNextInCheckStr player checkStr (pos+1) (checksLeft-1)
+    | (pos == len) = False
+    | otherwise = currCheck && checkNextInCheckStr player checkStr (pos+1) len (checksLeft-1)
     where currCheck = ((checkStr !! pos) == player)
+
+vertWin :: State -> Int -> Char -> Bool
+vertWin (State board colPos) playerMove player = 
+    let lastRow = ((colPos !! playerMove)+1)
+    in if lastRow < 4 then
+        (board !! (lastRow+1) !! playerMove) == player &&
+        (board !! (lastRow+2) !! playerMove) == player
+    else False
 
 test_board = [ ['*', '*', '*', '*', '*', '*', '*'], 
     ['*', '*', '*', '*', '*', '*', '*'],
     ['*', '*', '*', '*', '*', '*', '*'],
     ['*', '*', '*', '*', '*', '*', '*'],
-    ['d', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*']]
+    ['*', '*', '*', '*', '*', '*', '*'],
+    ['X', 'X', 'X', '*', '*', '*', '*']]
 
 -- win tests
 false_start = MoveRes [ ['*', '*', '*', '*', '*', '*', '*'], 
